7d7aa54d12b4f528315a1d9cccf6783191f3493d
reekyree <bl.luster.32@gmail.com>
2023-09-14 22:54:10 -0500

    Created main program file

---
+++ budget.py
@@ -0,0 +1,16 @@
+# Budget Application for the terminal.
+
+print('#############\n')
+print('BUDGET FRIEND\n')
+print('#############\n')
+
+
+def getNetIncome():
+
+    netIncome = input("Enter your net income: ")
+
+    print(f"Your net income is ${netIncome}.")
+    print("We'll use this to start creating a budget.")
+
+
+getNetIncome()
