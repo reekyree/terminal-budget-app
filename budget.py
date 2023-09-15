9e153c38c5f4415868149c9d9f08da03217e2387
reekyree <bl.luster.32@gmail.com>
2023-09-14 23:12:13 -0500

    Added rent and income functions

--- budget.py
+++ budget.py
@@ -7,10 +7,26 @@ print('#############\n')
 
 def getNetIncome():
 
-    netIncome = input("Enter your net income: ")
+    netIncome = int(input("Enter your net income: "))
 
     print(f"Your net income is ${netIncome}.")
     print("We'll use this to start creating a budget.")
+    return netIncome
+
+def getRentCost():
+    
+    print("Next, let's get your rent.\n")
+    rentCost = int(input("Next, enter the cost of rent per month: "))
+    return rentCost 
+
+income = getNetIncome()
+rent = getRentCost()
+
+percentage = rent / income
+oneThird = 0.33 
 
 
-getNetIncome()
+if percentage == oneThird:
+    print("Your rent is 1/3 of your total income. That's great!")
+else:
+    print(f"Your rent is {percentage:.2f}% of your budget. That's not great.")
